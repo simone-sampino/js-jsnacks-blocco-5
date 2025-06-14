f20dfd3be56b46d137347a417a70930ec34a224e
simone-sampino <s.samp98@gmail.com>
2025-06-14 17:10:44 +0200

    Add console logging for names and people in respective scripts

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,6 +3,17 @@ const names = ["Edoardo", "Simone", "Francesco"];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+// for
+console.log("CON for:");
+
+for (let i = 0; i < names.length; i++) {
+  const thisName = names[i];
+  console.log(thisName);
+}
+
+// forEach
+console.log("CON forEach:");
+
 names.forEach((names) => {
   console.log(names);
 });
