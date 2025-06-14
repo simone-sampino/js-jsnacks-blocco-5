f895ae2fc27de693dae6f5c5fd2dcebe00103942
simone-sampino <s.samp98@gmail.com>
2025-06-14 17:13:18 +0200

    Add console logging for names using for loop and forEach

--- snack-2/script.js
+++ snack-2/script.js
@@ -7,6 +7,17 @@ const people = [
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
 
+// for
+console.log("CON for:");
+
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(person.name);
+}
+
+// forEach
+console.log("CON forEach:");
+
 people.forEach((person) => {
   console.log(person.name);
 });
