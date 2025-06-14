13a22a4ece62cae39c5b06f5d085f1451dac9e2f
simone-sampino <s.samp98@gmail.com>
2025-06-14 17:35:48 +0200

    Add console logging for number increment examples using for, forEach, and map

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,31 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// for
+console.log("CON for:");
+
+const newArr = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  newArr.push(number + 1);
+}
+
+console.log(newArr);
+
+// forEach
+console.log("CON forEach:");
+
+const newArr2 = [];
+
+numbers.forEach((number) => newArr2.push(number + 1));
+
+console.log(newArr2);
+
+// map
+console.log("CON map:");
+
+const newArr3 = numbers.map((number) => number + 1);
+
+console.log(newArr3);
