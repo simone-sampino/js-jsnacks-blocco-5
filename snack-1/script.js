2fc998212a0add32454d66c631cdeb8b5c259305
simone-sampino <s.samp98@gmail.com>
2025-06-14 16:45:28 +0200

    Refactor names array and add console logging for each name

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((names) => {
+  console.log(names);
+});
