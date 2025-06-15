04de803c400e7cff59f6844e6276d30f8729ea70
simone-sampino <s.samp98@gmail.com>
2025-06-15 17:47:24 +0200

    Refactor student data logging examples using for, forEach, and find

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,23 +1,54 @@
 const students = [
   {
     id: 1,
-    name: 'Marco Lanci',
+    name: "Marco Lanci",
     age: 32,
-    class: '3C'
+    class: "3C",
   },
   {
     id: 2,
-    name: 'Mario Banfi',
+    name: "Mario Banfi",
     age: 34,
-    class: '4A'
+    class: "4A",
   },
   {
     id: 3,
-    name: 'Luigi Banzi',
+    name: "Luigi Banzi",
     age: 33,
-    class: '5B'
+    class: "5B",
   },
 ];
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// for
+console.log("CON for:");
+
+for (let i = 0; i < students.length; i++) {
+  const thisStudent = students[i];
+
+  if (thisStudent.name === "Marco Lanci") {
+    console.log(thisStudent.class);
+  }
+}
+
+// forEach
+console.log("CON forEach:");
+
+students.forEach((thisStudent) => {
+  if (thisStudent.name === "Marco Lanci") {
+    console.log(thisStudent.class);
+  }
+});
+
+// find
+console.log("CON find:");
+
+const marcoClass = students.find((thisStudent) => {
+  if (thisStudent.name === "Marco Lanci") {
+    return true;
+  }
+});
+
+console.log(marcoClass.class);
