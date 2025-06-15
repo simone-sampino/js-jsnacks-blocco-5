2467fdbe2d0d21914cc8a3a29b6f732554f50b7c
simone-sampino <s.samp98@gmail.com>
2025-06-15 16:26:25 +0200

    Add console logging for even number extraction using for, forEach, filter, and find

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,60 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// for
+console.log("CON for:");
+
+const even = [];
+
+for (let i = 0; i < nums.length; i++) {
+  const numb = nums[i];
+
+  if (numb % 2 === 0) {
+    even.push(numb);
+  }
+}
+
+console.log(even);
+
+// forEach
+console.log("CON forEach:");
+
+const even2 = [];
+
+nums.forEach((numb) => {
+  if (numb % 2 === 0) {
+    even2.push(numb);
+  }
+});
+
+console.log(even2);
+
+// filter
+console.log("CON filter:");
+
+const even3 = nums.filter((numb) => {
+  if (numb % 2 === 0) {
+    return true;
+  }
+});
+
+console.log(even3);
+
+// filter shortcut
+console.log("filter CON shortcut:");
+
+const even3_2 = nums.filter((numb) => numb % 2 === 0);
+
+console.log(even3_2);
+
+// find
+console.log("CON find:");
+
+const even4 = nums.find((numb) => {
+  numb % 2 === 0;
+  return numb;
+});
+
+console.log(even4);
+// ritorna SOLO il primo numero pari che trova, poi si ferma
